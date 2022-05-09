ffbb937b753ac77dde4c5ccfbb98c3be69b46def
krishna404 <agarwalbalkrishnaindia@gmail.com>
2022-05-10 00:12:35 +0530

    connect to db compeleted

--- server.js
+++ server.js
@@ -2,16 +2,31 @@ const express = require("express");
 const app = express();
 const port = 1729;
 // const path = require("path");
-// require('dotenv').config();
+require('dotenv').config();
 const {MongoClient} = require('mongodb')
 
 global.__basedir = __dirname;
 
 async function main() {
-    const client = MongoClient(process.env.CUSTOMCONNSTR_DB || process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient( process.env.CUSTOMCONNSTR_DB || process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    await client.connect();
-    await listDatabases(connect);
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch(e) {
+        console.error(e);
+    } finally {
+        client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(`- ${db.name}`));
 }
 
 const apiRoutes =  require("./routes/api.js");
