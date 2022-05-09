a2bba175ea60c2cfea911ec3adc88fbb83fcdfd0
krishna404 <agarwalbalkrishnaindia@gmail.com>
2022-05-09 23:56:35 +0530

    MongoDB connection setup

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 const port = 1729;
 // const path = require("path");
 // require('dotenv').config();
+const {MongoClient} = require('mongodb')
 
 global.__basedir = __dirname;
+
+async function main() {
+    const client = MongoClient(process.env.CUSTOMCONNSTR_DB || process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    await client.connect();
+    await listDatabases(connect);
+}
 
 const apiRoutes =  require("./routes/api.js");
 
